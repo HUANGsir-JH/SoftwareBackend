4f78974e1abbcff12c9f1305f5a4a957939f73f6
wenqiuyi <wenqiuyi2024@163.com>
2025-12-01 22:28:36 +0800

    update(content) 新增content需要的Mapper

--- service/service-content/src/main/java/org/software/content/mapper/ContentLikeFavoriteMapper.java
+++ service/service-content/src/main/java/org/software/content/mapper/ContentLikeFavoriteMapper.java
@@ -1,10 +1,13 @@
 package org.software.content.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.software.model.interaction.ContentLikeFavorite;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 public interface ContentLikeFavoriteMapper extends BaseMapper<ContentLikeFavorite> {
+    int delete(@Param("ew") Wrapper<ContentLikeFavorite> wrapper);
 }
