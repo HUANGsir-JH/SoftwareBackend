e1442ad04ff7891fe39cfc980f8554552628fff8
wenqiuyi <wenqiuyi2024@163.com>
2025-12-01 22:58:43 +0800

    Update（content）新增所需mapper

--- service/service-content/src/main/java/org/software/content/mapper/TagMapper.java
+++ service/service-content/src/main/java/org/software/content/mapper/TagMapper.java
@@ -1,7 +1,9 @@
 package org.software.content.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.software.model.content.tag.Tag;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,6 @@ import org.springframework.stereotype.Repository;
 */
 @Mapper
 public interface TagMapper extends BaseMapper<Tag> {
+
+    int delete(@Param("ew") Wrapper<Tag> wrapper);
 }
