f3deaf0b734660979a64a9a5d7ef9e300f33d416
wenqiuyi <wenqiuyi2024@163.com>
2025-12-01 22:29:33 +0800

    feat（content）新增content所需Mapper

---
+++ service/service-content/src/main/java/org/software/content/mapper/ContentMediaMapper.java
@@ -0,0 +1,12 @@
+package org.software.content.mapper;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.software.model.content.media.ContentMedia;
+
+@Mapper
+public interface ContentMediaMapper extends BaseMapper<ContentMedia> {
+    int delete(@Param("ew") Wrapper<ContentMedia> wrapper);
+}
