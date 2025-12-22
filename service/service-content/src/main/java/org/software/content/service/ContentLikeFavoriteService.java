307e32ead7390f51753e62fbb567119fdeef90d5
wenqiuyi <wenqiuyi2024@163.com>
2025-12-22 23:35:11 +0800

    update（interaction）修改请求参数

--- service/service-content/src/main/java/org/software/content/service/ContentLikeFavoriteService.java
+++ service/service-content/src/main/java/org/software/content/service/ContentLikeFavoriteService.java
@@ -5,12 +5,17 @@ import org.software.model.content.dto.ContentLikeFavoriteDTO;
 import org.software.model.content.vo.ContentLikeFavoriteVO;
 import org.software.model.exception.BusinessException;
 import org.software.model.interaction.ContentLikeFavorite;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface ContentLikeFavoriteService extends IService<ContentLikeFavorite> {
     boolean addOrCancelLike(ContentLikeFavoriteDTO dto) throws BusinessException;
-    List<ContentLikeFavoriteVO> getLikeFavoriteRecords(Integer userId, String type) throws BusinessException;
-    boolean readAll(Integer userId, String type) throws BusinessException;
-    List<ContentLikeFavoriteVO> getUnreadLikeFavorite(Integer userId, String type) throws BusinessException;
+    List<ContentLikeFavoriteVO> getLikeFavoriteRecords(Integer pageNum,
+                                                       Integer pageSize,
+                                                       String type) throws BusinessException;
+    boolean readAll() throws BusinessException;
+    List<ContentLikeFavoriteVO> getUnreadLikeFavorite(Integer pageNum,
+                                                      Integer pageSize,
+                                                      String type) throws BusinessException;
 }
