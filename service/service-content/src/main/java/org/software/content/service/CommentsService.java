a36c7c67c978535f4b21de8d6e9c7c9034025047
wenqiuyi <wenqiuyi2024@163.com>
2025-12-24 00:24:17 +0800

    update（comment）修改返回类型

--- service/service-content/src/main/java/org/software/content/service/CommentsService.java
+++ service/service-content/src/main/java/org/software/content/service/CommentsService.java
@@ -2,17 +2,20 @@ package org.software.content.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.software.model.content.dto.CommentDTO;
+import org.software.model.content.vo.CommentUnreadVO;
 import org.software.model.content.vo.CommentVO;
 import org.software.model.exception.BusinessException;
 import org.software.model.interaction.comment.Comments;
+import org.software.model.page.PageResult;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface CommentsService extends IService<Comments> {
     Long addComment(CommentDTO commentDTO) throws BusinessException;
     List<CommentVO> getRootComments(Long contentId) throws BusinessException;
-    List<CommentVO> getChildComments(Long parentCommentId) throws BusinessException;
-    List<CommentVO> getUnreadComments() throws BusinessException;
+    PageResult getChildComments(Integer rootCommentId, Integer pageNum, Integer pageSize) throws BusinessException;
+    List<CommentUnreadVO> getUnreadComments() throws BusinessException;
     Long getUnreadCommentCount() throws BusinessException;
 
     void deleteComments(Long commentId);
